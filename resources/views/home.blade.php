23ffe9a18e46be7e3ee9ef412d5c1cff0c944ebc
ShahzadeFortaxe <softpromani@gmail.com>
2026-06-09 01:45:31 +0530

    update fee

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -94,8 +94,8 @@
             <a href="https://www.cmclnmu.ac.in/admission-procedure" target="_blank" class="quick-link-badge"><i class="fas fa-file-alt"></i> Admission Form</a>
             <a href="https://lnmu.ac.in/academic-calendar" class="quick-link-badge" target="_blank"><i class="fas fa-calendar-alt"></i> Academic Calendar</a>
             <a href="https://www.cmclnmu.ac.in/college-research" class="quick-link-badge"target="_blank"><i class="fas fa-download"></i> Prospectus</a>
-            <a href="https://www.cmclnmu.ac.in/examination" class="quick-link-badge" target="_blank"><i class="fas fa-book-open"></i> Syllabus</a>
-            <a href="https://www.cmclnmu.ac.in/admission-procedure" class="quick-link-badge" target="_blank"><i class="fas fa-info-circle"></i> Fee Structure</a>
+            <a href="{{ asset('files/syllabus.pdf') }}" class="quick-link-badge" target="_blank"><i class="fas fa-book-open"></i> Syllabus</a>
+            <a href="{{asset('files/fee.pdf')}}" class="quick-link-badge" target="_blank"><i class="fas fa-info-circle"></i> Fee Structure</a>
             <a href="https://www.cmclnmu.ac.in/aqar" class="quick-link-badge" target="_blank"><i class="fas fa-file-pdf"></i> NCTE Documents</a>
         </div>
     </section>
